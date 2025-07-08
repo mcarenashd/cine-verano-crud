7edf46b25b2379be49a01bce765385782706c430
Camila Arenas <mcarenasdh@gmail.com>
2025-07-08 12:29:44 +0200

    Completed Get, Post and Delete Method

--- src/services.js
+++ src/services.js
@@ -1,82 +1,93 @@
 // Primero vamos a cargar en la página unas películas iniciales del DB.
-//Creamos una variable llamada fakeApiURL para facilidad si llegara existir un cambio solo sería cambiarlo ahí.
-const fakeApiURL = "http://localhost:3000/movies";
+//Creamos una variable llamada URL_API para facilidad si llegara existir un cambio solo sería cambiarlo ahí.
+const URL_API = "http://localhost:3000/movies";
 
 //Cuando se cargue la página necesitamos que se ejecute la función displayMovies que declararemos más adelante, y es la encargada de mostrar las películas.
 //También necesitamos qu cargue la función para agregar películas.
 document.addEventListener("DOMContentLoaded", () => {
-  fetch(fakeApiURL)
-    .then((res) => res.json())
-    .then((movies) => {
-      displayMovies(movies);
-    });
+  // Tu función para mostrar películas:
+  printMovies();
 });
 
-// La siguiente función tiene como objetivo mostrar las películas 
-function displayMovies(movies) {
-  const movieSlide = document.getElementById("movie-slide");  //Buscamos el div en el HTML donde vamos a meter las movieCards.
-  movies.forEach((movie) => {  //Empezamos a recorrer el array movies (archivo db.json, nuestra base de datos). 
-    const movieCard = document.createElement("div"); //Ahora vamos a crear un div dentro de nuestro HTML para cada movieCard. 
-    movieCard.className = "movie-card"; //El div movieCard va a tener un className 'movie-card'
-    movieCard.innerHTML = `  
-            <h1> ${movie.title}</h1><br>
-            <p><strong>Director:</strong>${movie.director} <p><br>
-            <p><strong>Género:</strong>${movie.genre} <p><br>
-            <p><strong>Año:</strong>${movie.year} <p><br>
-            <p><strong>País:</strong>${movie.country} <p><br>
-            <p><strong>Sinópsis:</strong>${movie.synopsis} <p><br>
-
-        `; // innerHTML nos permite insertar el código HTML que a tener el div movie-slide.
-    movieSlide.appendChild(movieCard); //movieCard va a quedar dentro del div ya creado en html movieSlide.
-  });
+//Funcion para Cargar las películas de la base de datos
+async function getAllMovies (){
+  const response = await fetch (URL_API)
+  const movieData = await response.json()
+  console.log(movieData)
+  return movieData
 }
 
+// METODO PRINT
+//Mostrar las películas en el HTML
+let moviesContainer =document.querySelector("section")
+async function printMovies(params) {
+    let movies = await getAllMovies();
+    moviesContainer.innerHTML =""
+    const movieList = movies.map(movie =>{
+        return moviesContainer.innerHTML += `<h1>${movie.title}</h1>
+        <p>${movie.director}</p>
+        <p>${movie.year}</p>
+        <p>${movie.country}</p>
+        <p>${movie.genre}</p>
+        <p>${movie.synopsis}</p>
+        <button onclick="deleteMovie('${movie.id}')">Eliminar</button>`
+    });
+    return movieList
+  }
 
+// DELETE
+async function deleteMovie(id) {
+  const response = await fetch(`${URL_API}/${id}`,{
+  method: "DELETE",
+  headers: {
+    'Content-Type':'application/json'
+  }
+  });
+  
+  if (response.ok){
+    await printMovies();
+  }
+  else {
+    console.log(`Error al cargar el libro`)
+  }
+};
 
+//POST
+async function newMovie(event) {
+  event.preventDefault();
 
+  const title = document.getElementById("title").value;
+  const director = document.getElementById("director").value;
+  const year = document.getElementById("year").value;
+  const country = document.getElementById("country").value;
+  const genre = document.getElementById("genre").value;
+  const synopsis = document.getElementById("synopsis").value;
+  
+  const newMovie = {
+    title,
+    director,
+    year,
+    country,
+    genre,
+    synopsis
+  };
 
+  const response = await fetch(URL_API,{
+  method: "POST",
+  headers: {
+    "Content-Type":"application/json"
+  },
+  body: JSON.stringify(newMovie)
+  });
+  
+  if (response.ok){
+    await printMovies();
+  }
+  else {
+    console.log(`Error al cargar el libro`)
+  }
+};
 
-
-// function displayMovies(movies) {
-//     const movieSlide = document.getElementById('movie-slide')
-//     movieSlide.innerHTML = '' // Limpiamos el contenido anterior
-
-//     movies.forEach(movie => {
-//         // Creamos un elemento div para cada película
-//         const movieCard = document.createElement('div')
-//         movieCard.className = 'movie-card'
-//         movieCard.innerHTML = `
-//             <h3>${movie.title}</h3>
-//             <p><strong>Director:</strong> ${movie.director}</p>
-//             <p><strong>Año:</strong> ${movie.year}</p>
-//             <p><strong>País:</strong> ${movie.country}</p>
-//             <p><strong>Género:</strong> ${movie.genre}</p>
-//             <p><strong>Sinopsis:</strong> ${movie.synopsis}</p>
-//             <button onclick="deleteMovie(${movie.id})">Eliminar</button>
-//         `
-//         movieSlide.appendChild(movieCard)
-//     })
-// }
-
-// function getMovies(){
-//     fetch(fakeApiURL, { //Hacemos una petición HTTP a la URL donde está nuestra fake API.
-//         method: 'GET'  //El tipo de método que vamos a usar es GET.
-//     })
-
-// .then(response => {
-//     // Verificamos si la respuesta es exitosa
-//     if (!response.ok) {
-//         throw new Error('Error al obtener las películas')
-//     }
-//     return response.json() // Convertimos la respuesta a JSON
-// })
-// .then(movies => {
-//     // Aquí recibimos el array de películas
-//     console.log('Películas obtenidas:', movies)
-//     displayMovies(movies) // Llamamos a la función para mostrarlas
-// })
-// .catch(error => {
-//     console.error('Error:', error)
-//     alert('Error al cargar las películas')
-// })
-// }
+// Escuchar el envío del formulario
+const form = document.getElementById("movieForm");
+form.addEventListener("submit", newMovie)
